dd6f556ed27b4e41bc3484d6a4cfded022def5ee
Jamal H <Vega.cf16@gmail.com>
2025-01-11 22:28:23 -0500

    Final changes

--- src/main/java/frigidoven/betterspikes/BetterSpikes.java
+++ src/main/java/frigidoven/betterspikes/BetterSpikes.java
@@ -3,13 +3,31 @@ package frigidoven.betterspikes;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.util.GameStartEntrypoint;
+import turniplabs.halplibe.util.RecipeEntrypoint;
 
 
-public class BetterSpikes implements ModInitializer {
+public class BetterSpikes implements ModInitializer, GameStartEntrypoint, RecipeEntrypoint {
     public static final String MOD_ID = "betterspikes";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitialize() {
         LOGGER.info("BetterSpikes initialized. Yippee!");
     }
+
+	@Override
+	public void beforeGameStart() {
+	}
+
+	@Override
+	public void afterGameStart() {
+	}
+
+	@Override
+	public void onRecipesReady() {
+	}
+
+	@Override
+	public void initNamespaces() {
+	}
 }
